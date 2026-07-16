6ed2b8e90fc354291baa59cf7924106a30c4ff70
Shamil <shamilsalam777@gmail.com>
2026-07-16 21:11:07 +0530

    implement task list and create task. Phase 3 Completed

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { useTasks } from "./hooks/useTasks";
+import { useCreateTask } from "./hooks/useCreateTask";
+
 export default function Home() {
+  const [title, setTitle] = useState("");
+
+  const { data: tasks } = useTasks();
+
+  const createTask = useCreateTask();
+
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold">
+    <main className="max-w-xl mx-auto mt-10 space-y-4">
+
+      <h1 className="text-3xl font-bold">
         TaskFlow
       </h1>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          createTask.mutate(title);
+
+          setTitle("");
+        }}
+      >
+        <input
+          className="border p-2 w-full"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Enter task..."
+        />
+      </form>
+
+      <div className="space-y-2">
+        {tasks?.map((task) => (
+          <div
+            key={task.id}
+            className="border rounded p-3"
+          >
+            {task.title}
+          </div>
+        ))}
+      </div>
+
     </main>
   );
 }
--- frontend/app/services/task.service.ts
+++ frontend/app/services/task.service.ts
@@ -1,5 +1,17 @@
 import { api } from "../lib/axios";
+import { Task } from "../types/task";
 
 export const taskService = {
-    getTasks: () => api.get("/tasks"),
+  async getTasks() {
+    const { data } = await api.get<Task[]>("/tasks");
+    return data;
+  },
+
+  async createTask(title: string) {
+    const { data } = await api.post("/tasks", {
+      title,
+    });
+
+    return data;
+  },
 };
